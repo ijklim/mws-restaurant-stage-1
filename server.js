773d0e343a345dd6b9d065c001afa09e17d0f8d6
Ivan Lim <aimprove@gmail.com>
2018-05-15 09:52:08 -0400

    chore: Install Express server

---
+++ server.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+app.use('/', express.static(process.cwd() + '/'));
+
+let port = process.env.PORT || 80;
+app.listen(port, function () {
+  console.log(`Node.js listening on port ${port}...`);
+});
