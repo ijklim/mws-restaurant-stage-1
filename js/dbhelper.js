46302f6738b570aaeb94ed83a1a9bf7a6b3e0ab5
Ivan Lim <aimprove@gmail.com>
2018-05-17 13:57:58 -0400

    fix: Use relative image path

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -145,7 +145,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}`);
+    return (`./img/${restaurant.photograph}`);
   }
 
   /**
