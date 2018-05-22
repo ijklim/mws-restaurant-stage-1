34aacf3ceb8fa33c49ea1973680c04dd68884c65
Ivan Lim <aimprove@gmail.com>
2018-05-22 13:29:20 -0400

    feat: Delete obsolete caches

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-let CACHE_NAME = 'mws-restaurant-v2';
+let CACHE_NAME = 'mws-restaurant-v7';
 
 self.addEventListener('fetch', function(event) {
+  // console.log(`[Comment] fetch event listener: ${event.request.url}`);
   event.respondWith(caches.open(CACHE_NAME).then(cache => {
     return cache.match(event.request)
       .then(response => {
@@ -12,15 +13,32 @@ self.addEventListener('fetch', function(event) {
         // console.log(`[Comment] Fetching from server: ${event.request.url}`);
         return fetch(event.request.url, { mode: 'no-cors' })
           .then(response => {
+            // console.log(`[Comment] fetch returned: ${response.headers.}`);
             cache.put(event.request.url, response.clone());
             return response;
           })
           .catch(error => {
-            return new Response('Network is down');
+            return new Response(`Network is down, failed to fetch ${event.request.url}`);
           });
       })
       .catch(error => {
         return new Response(`Cache match has encountered an error: ${error}`);
       });
   }));
+});
+
+// Life Cycle: install
+self.addEventListener('install', function(event) {
+  // console.log(`[Comment] Life cycle event 'install' triggered`);
+
+  // Delete obsolete caches
+  caches.keys()
+    .then(keys => {
+      keys.forEach(key => {
+        if (CACHE_NAME === key) return;
+
+        console.log(`Deleting old cache '${key}'`);
+        caches.delete(key);
+      });
+    });
 });
