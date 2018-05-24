f66f1375004bcc49287e87787babf961cde347aa
Ivan Lim <aimprove@gmail.com>
2018-05-23 22:28:55 -0400

    refactor: Move lazy load image logic to main.js

--- js/main.js
+++ js/main.js
@@ -70,17 +70,24 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
  * Initialize Google map, called from HTML.
  */
 window.initMap = () => {
-  let loc = {
-    lat: 40.722216,
-    lng: -73.987501
-  };
-  self.map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 12,
-    center: loc,
-    scrollwheel: false
-  });
-  document.getElementById('map').setAttribute('role', 'application');
   updateRestaurants();
+  lazyLoadImages();
+
+  // Avoid blocking critical path to improve performance score
+  window.setTimeout(() => {
+    let loc = {
+      lat: 40.722216,
+      lng: -73.987501
+    };
+
+    self.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: loc,
+      scrollwheel: false
+    });
+
+    document.getElementById('map').setAttribute('role', 'application');
+  }, 1500);
 }
 
 /**
@@ -184,4 +191,48 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
+}
+
+lazyLoadImages = () => {
+  // Lazy load images
+  let lazyImages = [];
+
+  window.addEventListener('load', event => {
+    // Lazy load images
+    // Note: Use Array.prototype.slice.call to convert HTMLCollection to array
+    lazyImages = Array.prototype.slice.call(document.getElementsByClassName('restaurant-img'));
+    // console.log('[Comment] Found ' + lazyImages.length + ' lazy images');
+  });
+
+  function lazyLoad () {
+    lazyImages.forEach(image => {
+      if (!isInViewport(image)) return;
+
+      if (image.getAttribute('data-src')) {
+        console.log(`[Comment] Lazy loading ${image.getAttribute('data-src')}`);
+        image.src = image.getAttribute('data-src');
+        image.removeAttribute('data-src');
+      }
+    });
+
+    cleanLazyImages();
+  };
+
+  function isInViewport (el) {
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.bottom >= 0 &&
+        rect.right >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  function cleanLazyImages () {
+    lazyImages = lazyImages.filter(image => image.getAttribute('data-src'));
+  }
+
+  window.addEventListener('load', lazyLoad);
+  window.addEventListener('scroll', lazyLoad);
 }
